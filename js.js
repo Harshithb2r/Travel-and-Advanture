19f53bc5ad6a1368a402f4cd31e4c937310623a5
Harshithb2r <117143505+Harshithb2r@users.noreply.github.com>
2023-06-27 09:16:19 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,89 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.add('active')
+}
+
+document.querySelector('#nav-close').onclick = () => {
+    navbar.classList.remove('active')
+}
+
+let searchform = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchform.classList.add('active')
+}
+
+document.querySelector('#close-search').onclick = () => {
+    searchform.classList.remove('active')
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2, 
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2, 
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+    },
+});
